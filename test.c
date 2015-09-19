6f39279e09d12178b9d436ed4105e7d960e3171e
nedim.malja <nedim.malj@gmail.com>
2015-09-19 22:48:38 +0200

    merged test.x

--- test.c
+++ test.c
@@ -1,7 +1,4 @@
 # this is a test file
 # adding the second line
-<<<<<<< HEAD
 # adding a line to file on newbranch
-=======
 # adding some other text to the third line to see what the merge will do
->>>>>>> mybranch
