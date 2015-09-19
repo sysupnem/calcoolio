65326c2552c1f5503f14f64cfa80780eba560565
nedim.malja <nedim.malj@gmail.com>
2015-09-19 22:53:41 +0200

    deleting test files

--- test.c
+++
@@ -1,4 +0,0 @@
-# this is a test file
-# adding the second line
-# adding a line to file on newbranch
-# adding some other text to the third line to see what the merge will do
--- test3.c
+++
@@ -1,4 +0,0 @@
-this is the second file
-adding another line but only to newbranch
-
-adding a new line to test3.c
